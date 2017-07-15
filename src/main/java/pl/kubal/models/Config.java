51626ac1a2071aba447448c97a0c2464b6237cd4
Kuba Leszczynski <kuba_leszczynski@wp.pl>
2017-07-15 13:19:35 +0200

    weather service, capturing first data

---
+++ src/main/java/pl/kubal/models/Config.java
@@ -0,0 +1,10 @@
+package pl.kubal.models;
+
+/**
+ * Created by Jim on 2017-07-15.
+ */
+public class Config {
+    public static  final String APPID = "28261a18f61f80e43eff1e368778855c"; //klucz do api z openweathermap;
+
+    public static  final String APPURL = "http://api.openweathermap.org/data/2.5/"; //URL do api z openweathermap;
+}
